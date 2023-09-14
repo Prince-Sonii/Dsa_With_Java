a987aab6061b0e42d4779ba268bdc37e0886d50e
prince <princesoni200325@gmail.com>
2023-09-15 02:10:38 +0530

    code on arry

---
+++ array.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        //array accessing elements by for each loop
+        /*int [] arr = {1,2,3,4,5,6,7};
+        for(int element:arr) {
+            System.out.println(element);
+            */
+        // sorting an array then findind index of given number
+        int [] myArray = {10,2,55,67,32,12};
+        //sorting of aaray
+        Arrays.sort(myArray);
+        int toFindIndexOf = 55;
+        int indexValue = Arrays.binarySearch(myArray,toFindIndexOf);
+        if (indexValue<0) {
+            System.out.println("Given value is not present in array");
+        }
+        else {
+            System.out.println(toFindIndexOf + "'s index is = " + indexValue);
+        }
+    }
+}
