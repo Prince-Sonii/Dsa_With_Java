53f3daf28125378058859afb5fe46801e59e6748
prince <princesoni200325@gmail.com>
2024-03-26 22:33:34 +0530

    added rhombus and 1 more shape + some code refining

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,6 @@
 public class Pattern {
     public static void main(String[] args) {
-        patternTriangle(5);
-        leftTipMountain(9);
+        rhombus(4);
     }
     static void patternTriangle(int n) {
        /*
@@ -68,23 +67,26 @@ public class Pattern {
 
          */
 
-        for (int row =1;row<=n;row++) {
-            int half = n/2;
-            if(row<=half+1) {
-                for (int col1=1;col1<=row;col1++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
-            }else {
-                for (int col2 =1;col2<=n+1-row;col2++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
+        for (int row =1;row<2*n;row++) {
+            int half = row<=n?row:2*n-row;
+            for (int col=1;col<=half;col++){
+                System.out.print("* ");
             }
+            System.out.println();
         }
-    }
 
-    static void leftTipMountain(int n) {
+    }
+//            if(row<=half+1) {
+//                for (int col1=1;col1<=row;col1++) {
+//                    System.out.print("* ");
+//                }
+//                System.out.println();
+//            }else {
+//                for (int col2 =1;col2<=n+1-row;col2++) {
+//                    System.out.print("* ");
+//                }}
+
+    static void leftTipSidewaysMountain(int n) {
         /*
 
              *
@@ -124,5 +126,32 @@ public class Pattern {
         }
     }
 
+    static void leftSideTriangle(int n) {
+        for(int row =1;row<=n;row++) {
+            for (int col =1;col<=n;col++) {
+                if(col>n-row) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
 
-}
+        }
+    }
+    static void rhombus(int n) {
+        for (int row =1;row<2*n;row++) {
+            for (int col=1;col<=n;col++) {
+                if (n>=row&&col>n-row || row>n&&col>row-n){
+                    System.out.print("* ");
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+    }
+
+
+
+}}
