17c13f4b9271ebd408342d06631607a181e0255d
prince <princesoni200325@gmail.com>
2024-04-15 20:35:00 +0530

    more refined code

--- PalindromeBreak.java
+++ PalindromeBreak.java
@@ -1,33 +1,27 @@
 class PalindromeBreak {
     //leetcode 1328s
     public static void main(String[] args) {
-        String name = "aaaaaa";
-        String ans = breakPalindrome(name);
-        System.out.println(ans);
+        String testCase1 = "abccba";
+        String testCase2 = "aa";
+        System.out.println(breakPalindrome(testCase1));
+        System.out.println(breakPalindrome(testCase2));
+
 
     }
     static String breakPalindrome(String palindrome) {
-        int i=0;
-        int replace =0;
-        int mid = palindrome.length()/2;
         StringBuilder ans = new StringBuilder();
         ans.append(palindrome,0,palindrome.length());
-        while(i<palindrome.length()) {
-            if (palindrome.length()==1) {
-                break;
-            }
-            if(i==mid) {
-                replace = palindrome.charAt(palindrome.length()-1)+1;
-                ans.setCharAt(palindrome.length()-1,(char)replace);
-                return ans.toString();
-            }
+        if (palindrome.length()<2) {
+            return new String("");
+        }
+
+        for(int i=0;i<=palindrome.length()/2;i++) {
             if(palindrome.charAt(i)!='a') {
                 ans.setCharAt(i,'a');
                 return ans.toString();
-            } else {
-                i++;
             }
         }
-        return new String("");
+        ans.setCharAt(palindrome.length()-1,'b');
+        return ans.toString();
     }
 }
