fce3447a1fe9064b10c10557e64363b34dd162a3
prince <princesoni200325@gmail.com>
2024-05-20 22:55:04 +0530

    Solved the same question with O(logN) complexity

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,34 +1,29 @@
 public class SearchInsertPosition {
     // leet code question no.35
     /*Given a sorted array of distinct integers and a target value,
-    return the index if the target is found. If not, 0return the
+    return the index if the target is found. If not, return the
     index where it would be if it were inserted in order
      */
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5};
-        int [] nums2 = {1,5,6};
-        int res1 = searchInsert(nums, 2);
-        int res2 = searchInsert(nums2,7);
-        System.out.println(res1);
-        System.out.println(res2);
-
-
+        int [] nums = {1,3,5,6};
+        int target =5;
+        System.out.println(searchInsert(nums,target));
     }
     //solution
     static int searchInsert(int[] nums, int target) {
-        // finding the target by traversing the array
-        for(int i=0;i<nums.length;i++ ) {
-            if(nums[i]==target) {
-                return i;
+        int s=0;
+        int e=nums.length-1;
+        while(s<=e) {
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }
+            if(nums[mid]>target) {
+                e=mid-1;
+            } else{
+                s=mid+1;
             }
         }
-        // code for inserting the target at correct indedx if it is not in array
-        for(int i=0;i<nums.length;i++) {
-            if(target<nums[i]) {
-                return i;
-            }
-        }
-        //if the target is greater than all numbers in array then it will be inserted at the last
-        return nums.length;
+        return s;
     }
 }
