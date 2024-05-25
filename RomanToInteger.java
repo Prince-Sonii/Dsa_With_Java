daa5f3933a30ff2b303ef153d5ad533f21bb1e91
prince <princesoni200325@gmail.com>
2024-05-25 22:27:32 +0530

    Leet code Qno. 13

---
+++ RomanToInteger.java
@@ -0,0 +1,21 @@
+class RomanToInteger {
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III"));
+    }
+    static int romanToInt(String s) {
+        int [] num = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String [] roman = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String p =s;
+        int ans =0;
+        while(!p.isEmpty()){
+            for(int i=0;i<num.length;i++) {
+                if(p.startsWith(roman[i])) {
+                    ans+=num[i];
+                    p=p.substring(roman[i].length());
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
