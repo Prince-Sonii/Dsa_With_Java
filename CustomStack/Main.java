5d1065b46fa75e92f54d53d5f85ab59deac27148
prince <princesoni200325@gmail.com>
2024-06-15 15:48:25 +0530

    Implementing the custom stack

---
+++ CustomStack/Main.java
@@ -0,0 +1,24 @@
+package CustomStack;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        CustomStack stack = new CustomStack(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+}
