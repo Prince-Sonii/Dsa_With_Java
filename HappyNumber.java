45f9ac29a9b84d7cd02109b3bb600d1051d5dd6c
prince <princesoni200325@gmail.com>
2024-06-24 11:22:10 +0530

    Leet code Qno.202

---
+++ HappyNumber.java
@@ -0,0 +1,26 @@
+class HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(12));
+    }
+    static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do{
+            slow = square(slow);
+            fast = square(square(fast));
+        } while(slow!=fast);
+        if(slow==1) {
+            return true;
+        }
+        return false;
+    }
+    static int square(int n) {
+        int ans=0;
+        while(n>0) {
+            int rem = n%10;
+            ans+= rem*rem;
+            n/=10;
+        }
+        return ans;
+    }
+}
