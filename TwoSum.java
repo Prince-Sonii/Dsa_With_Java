ad42506b8d586eb91c2428fde61b825effb27c2a
prince <princesoni200325@gmail.com>
2024-06-28 16:14:55 +0530

    optimised solution of leet code Qno.1 than beofre

--- TwoSum.java
+++ TwoSum.java
@@ -7,14 +7,30 @@ class TwoSum {
         System.out.println(Arrays.toString(twoSum(nums,target)));
     }
     static int[] twoSum(int[] nums, int target) {
-        for(int i =1;i<nums.length;i++){
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]+nums[j-i] == target) {
-                    return new int[]{j,j-i};
-                }
+        int[] arr = Arrays.copyOf(nums,nums.length);
+        int start=0;
+        int end =arr.length-1;
+        int index1=-1;
+        int index2=-1;
+        Arrays.sort(arr);
+        while(start<arr.length && end>-1) {
+            if(arr[start]+arr[end]==target) {
+                break;
+            }
+            if(arr[start]+arr[end]>target) {
+                end--;
+            } else {
+                start++;
             }
         }
-        return new int []{};
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]==arr[start] && index1<0) {
+                index1=i;
+            } else if(nums[i]==arr[end] && index2<0) {
+                index2=i;
+            }
+        }
+        return new int[]{index1, index2};
         
     }
 }
