b6dea432b8bd7f078370a135d8547252b256762f
prince <princesoni200325@gmail.com>
2024-07-04 17:02:16 +0530

    Leet code Qno.287 optimised than before and within given constraints

--- DuplicateNumber.java
+++ DuplicateNumber.java
@@ -1,21 +1,12 @@
 class DuplicateNumber {
     public int findDuplicate(int[] nums) {
-        for(int i=0;i<nums.length;) {
-            int correct = nums[i]-1;
-            if(nums[i]!=nums[correct]) {
-                int temp = nums[i];
-                nums[i] = nums[correct];
-                nums[correct] = temp;
-            } else {
-                i++;
+        for(int i=0;i<nums.length;i++) {
+            int ind = Math.abs(nums[i]);
+            if(nums[ind] < 0) {
+                return ind;
             }
+            nums[ind] = -nums[ind];
         }
-        int ans =0;
-        for(int index=0;index<nums.length;index++) {
-            if(nums[index]!=index+1) {
-                ans = nums[index];
-            }
-        }
-        return ans;
+        return -1;
     }
 }
